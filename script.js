41d05401aeadc410072c86a63c1ac99b93ddad47
Constantin Tews <constantin.tews@gmail.com>
2022-08-15 15:43:47 +0200

    Remove .htaccess bc github pages doesn't support it. Remove script.js bc is has no usings anymore.

--- script.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-    if (window.location.protocol != "https:")
-        window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-})();
